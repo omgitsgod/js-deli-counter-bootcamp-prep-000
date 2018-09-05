3577417185fc0617912c88fba4f0928400b71c62
Ryan Travis <rpenchenski@gmail.com>
2018-09-05 00:42:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function takeANumber(current, neww) {
 function nowServing(line) {
   if (line.length > 0) {
     return `Currently serving ${line[0]}.`
+    line.shift()
   } else {
     return "There is nobody waiting to be served!"
   }
