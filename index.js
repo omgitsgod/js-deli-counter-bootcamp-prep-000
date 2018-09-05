a940486c84cdc10cc863e05e1f641b788da0bb67
Ryan Travis <rpenchenski@gmail.com>
2018-09-05 01:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,21 +16,12 @@ function nowServing(line) {
 
 function currentLine(line) {
   
-  var blah = []
-  if (line.length > 0) {
-  for (var i = 0; i < line.length; i++) {
-    blah.push(`${i+1}. ${line[i]}`)
-    return `The line is currently ${blah}`
-  }
-  } else {
-    return "The line is currently empty."
-  }
-}
-
-if (!line.length) {
+  if (!line.length) {
   return "The line is currently empty."
 }
 blah = []
 for (var i =0; i < line.length; i++) {
   blah.push(`${i+1}. $line[i]}`)
+  return `the line is currently ${blah}`
+}
 }
