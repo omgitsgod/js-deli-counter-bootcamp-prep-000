b985fdaaf2755a034f160d91b63d12114f59acee
Ryan Travis <rpenchenski@gmail.com>
2018-09-05 00:36:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving line[0].`
+    return `Currently serving ${line[0]}.`
   }
 }
